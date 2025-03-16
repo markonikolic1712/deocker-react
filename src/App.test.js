350dcd68d305e0920086847dab19311992bce49e
markonikolic1712 <nikolic.marko11@gmail.com>
2025-03-16 10:34:09 +0100

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn rea1ct link", () => {
+test("", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
